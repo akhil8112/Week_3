d48ba566acf5c251e57140cb43c51dc66e7fefb2
akhil8112 <akhilsingh8113@gmail.com>
2025-09-14 22:17:26 +0530

    commit

--- api/app.py
+++ api/app.py
@@ -105,6 +105,12 @@ def home():
         company_names=company_names
     )
 
+from flask import Flask, jsonify
 
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return jsonify(message="Hello from Flask on Vercel!")
 if __name__ == "__main__":
     app.run(debug=True)
